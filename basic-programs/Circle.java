a83122edd4b5d651cdc7cda932805bd4223c28ee
pankajkumar0202 <pankaj476987@gmail.com>
2026-01-11 15:04:42 +0530

    Area of a Circle

---
+++ basic-programs/Circle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Circle{
+
+     public static void main(String[] args){
+         Scanner sc = new Scanner(System.in);
+         double r = sc.nextDouble();
+		 double a = 3.141 + r * r;
+         System.out.println(a);
+         sc.close();
+        }
+    }
