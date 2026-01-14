e9226cbccfed51d9e4999b0c5bd770396643a43d
pankajkumar0202 <pankaj476987@gmail.com>
2026-01-14 23:04:47 +0530

    KmToMil

---
+++ basic-programs/KmToMil.java
@@ -0,0 +1,8 @@
+public class KmToMil{
+      public static void main(String[] args){
+         double km = 10.8;
+		 double kmpermiles = 1.6;
+		 double miles = km * kmpermiles; 
+	     System.out.println("The distance "+ km +" in miles is " + miles );
+		}
+    }
