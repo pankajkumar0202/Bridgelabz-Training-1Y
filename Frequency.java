1bf18b7022c98bac0a55ea5dd0b392764f2aeda3
pankajkumar0202 <pankaj476987@gmail.com>
2026-02-06 11:58:23 +0530

    [pankaj] Add.frequency of each element

---
+++ Frequency.java
@@ -0,0 +1,21 @@
+class Frequency{
+      public static void main(String[]args){
+          int [] arr = {1,2,3,4,1,4,3,2,1,4};  
+          boolean [] visited = new boolean[arr.length];
+          for(int i = 0; i<arr.length; i++){
+		      if(visited[i]){
+		          continue;
+		        }
+              int count = 1;
+              visited[i] = true;
+              for(int j = i+1; j<arr.length; j++){
+                  if(arr[i]==arr[j]){
+                      count++;
+                      visited[j] = true;
+                    }
+			    }
+				System.out.println("frequency of " +arr[i]+ " is " +count);
+            }
+        }
+    }
+
