7fa4b998444d56bae4a1bd1b96d803e159adaba0
Pankaj Kumar Rajput <pankaj476987@gmail.com>
2026-02-11 22:08:53 +0530

    ['Pankaj]Add. DateFormat program

---
+++ src/com/gla/string/extras1/DateFormat.java
@@ -0,0 +1,15 @@
+package com.gla.string.extras1;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+public class DateFormat{
+    public static void main(String[] args){
+        LocalDate d=LocalDate.now();
+        DateTimeFormatter f1=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter f2=DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter f3=DateTimeFormatter.ofPattern("EEE, MMM dd, yyyy");
+        System.out.println(d.format(f1));
+        System.out.println(d.format(f2));
+        System.out.println(d.format(f3));
+    }
+}
