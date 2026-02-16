ad5c2aa907b1d60d4cc8101de42092c1803eed74
Pankaj Kumar Rajput <pankaj476987@gmail.com>
2026-02-16 23:39:30 +0530

    [Pankaj] Add.Palindrome program

---
+++ src/com/gla/classes-objects/level2/Palindrome.java
@@ -0,0 +1,29 @@
+package com.gla.classes-objects.level2;
+
+class Palindrome {
+    String text;
+    PalindromeChecker(String text) {
+        this.text = text;
+    }
+    boolean isPalindrome() {
+        String reversed = "";
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversed += text.charAt(i);
+        }
+        return text.equalsIgnoreCase(reversed);
+    }
+    void displayResult() {
+        if (isPalindrome()) {
+            System.out.println(text + " is a Palindrome.");
+        } else {
+            System.out.println(text + " is NOT a Palindrome.");
+        }
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+
+        PalindromeChecker p = new PalindromeChecker("madam");
+        p.displayResult();
+    }
+}
