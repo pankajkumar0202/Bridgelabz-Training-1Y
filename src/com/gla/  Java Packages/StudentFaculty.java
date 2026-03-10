b3181cf225135031f05b4129ad3fc83d42d57366
Pankaj Kumar Rajput <pankaj476987@gmail.com>
2026-03-10 22:31:22 +0530

    [Pankaj] Add. Student Faculty program

---
+++ src/com/gla/  Java Packages/StudentFaculty.java
@@ -0,0 +1,35 @@
+class Student {
+
+    public void displayStudent() {
+        String name = "Rahul";
+        int rollNo = 101;
+
+        System.out.println("Student Name: " + name);
+        System.out.println("Roll Number: " + rollNo);
+    }
+}
+
+class Faculty {
+
+    public void displayFaculty() {
+        String name = "Dr. Sharma";
+        String subject = "Computer Science";
+
+        System.out.println("Faculty Name: " + name);
+        System.out.println("Subject: " + subject);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student s = new Student();
+        Faculty f = new Faculty();
+
+        s.displayStudent();
+        System.out.println();
+
+        f.displayFaculty();
+    }
+}
