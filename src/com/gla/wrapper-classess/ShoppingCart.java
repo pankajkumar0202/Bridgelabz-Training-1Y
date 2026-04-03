4d2686b51121aa863c1a199d67fa48b14e2bc54a
Pankaj Kumar Rajput <pankaj476987@gmail.com>
2026-04-03 19:57:06 +0530

    [Pankaj] Add. Shopping Cart Price Calculation

---
+++ src/com/gla/wrapper-classess/ShoppingCart.java
@@ -0,0 +1,16 @@
+package com.gla.wrapper-classess;
+
+public class ShoppingCart {
+    public static void main(String[] args) {
+        String[] arr = {"250", "499", "abc", "99"};
+        int total = 0;
+        for (String i : arr) {
+            try {
+                total += Integer.parseInt(x);
+            } catch (Exception e) {
+               }
+          }
+
+        System.out.println(total);
+    }
+}
