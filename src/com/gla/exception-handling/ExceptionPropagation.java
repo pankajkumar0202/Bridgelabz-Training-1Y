38a781dfee6d5435173aa9926c013d1de6966370
Pankaj Kumar Rajput <pankaj476987@gmail.com>
2026-04-05 00:16:33 +0530

    [Pankaj] Add. Exception Propagation program

---
+++ src/com/gla/exception-handling/ExceptionPropagation.java
@@ -0,0 +1,22 @@
+public class ExceptionPropagation {
+
+    static double calculateInterest(double amount, double rate, int years)
+            throws IllegalArgumentException {
+
+        if (amount < 0 || rate < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return (amount * rate * years) / 100;
+    }
+
+    public static void main(String[] args) {
+        try {
+            double result = calculateInterest(1000, -5, 2);
+            System.out.println(result);
+        }
+        catch (IllegalArgumentException exp) {
+            System.out.println("Invalid input: Amount and rate must be positive");
+        }
+    }
+}
