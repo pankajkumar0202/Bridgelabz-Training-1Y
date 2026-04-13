77fb6618f7802631a58875322713bf6e9e151113
Pankaj Kumar Rajput <pankaj476987@gmail.com>
2026-04-13 20:49:55 +0530

    [Pankaj] Add. Generic Box program

---
+++ src/com/gla/generics-collections/GenericBox.java
@@ -0,0 +1,20 @@
+package com.gla.generics-collections;
+
+class GenericBox {
+    private T value;
+
+    void set(T value) {
+        this.value = value;
+    }
+    T get() {
+        return value;
+    }
+}
+class main {
+    static void main() {
+        Box<Integer> b1 = new Box<>();
+        b1.set(10);
+        Box<String> b2 = new Box<>();
+        b2.set("Hello World");
+    }
+}
